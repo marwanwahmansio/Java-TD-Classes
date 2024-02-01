184c108d4ae5a709787d27f33db3a688fed7c5ff
marwanwahmansio <144268508+marwanwahmansio@users.noreply.github.com>
2024-02-01 21:53:16 +0100

    Add files via upload

---
+++ AppWest.java
@@ -0,0 +1,8 @@
+public class AppWest {
+    public static void main( String [] args ){
+        Personnage heros = new Personnage( "Jo" , true , 2 ) ;
+        System.out.println( heros.getNom() ) ;
+        heros.seReposer() ;
+        System.out.println( heros.getFatigue() ) ;
+    }
+}
